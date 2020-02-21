f101c65d39c9d931a6457e15fcde3ac7ca7d8d1d
arazosman <arazosman@outlook.com>
2020-02-22 02:56:41 +0300

    ADD: Project added.

---
+++ Assets/Scripts/AtomMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtomMovement : MonoBehaviour
+{
+    public Direction direction;
+    private Vector3 initPos;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initPos = transform.position;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (rb.velocity.x == 0)
+            CheckInput();
+        else
+            CheckEdges();
+    }
+
+    private void CheckInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (transform.position.x < 0)
+                rb.velocity = new Vector2(10, 0);
+            else
+                rb.velocity = new Vector2(-10, 0);
+        }
+    }
+
+    private void CheckEdges()
+    {
+        if (transform.position == initPos || (direction == Direction.Right && transform.position.x > 0) || (direction == Direction.Left && transform.position.x < 0))
+            rb.velocity = Vector2.zero;
+    }
+}
---
+++ Assets/Scripts/Scrolling.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scrolling : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public Transform rocketPos;
+	float initialY;
+	private Vector2 initPos;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(0, -5f);
+        initialY = rocketPos.position.y;
+		initPos = transform.position;
+    }
+
+    private void FixedUpdate()
+	{
+		if (transform.position.y < initialY)
+			transform.position = initPos;
+	}
+}
